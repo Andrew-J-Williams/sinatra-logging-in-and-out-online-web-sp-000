55be86b0b2e6197e3a52a8bfc861911a20c67a62
Andrew Williams <1williamsaj@gmail.com>
2020-01-23 02:29:11 +0000

    Automatically backed up by Learn

--- db/migrate/20200123022849_user.rb
+++ db/migrate/20200123022849_user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Migration[5.1]
   def change
+    def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.decimal :balance
+    end
+
   end
 end
